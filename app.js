a88734c17fbfe91e9cf8017b53d9433ccdeef8fc
b0totmat <t0thm4t3@gmail.com>
2024-12-05 17:11:27 +0100

    Body-Parser nem kell

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import express from "express"
-import bodyParser from "body-parser"
 import sqlite3 from 'sqlite3'
 
 const app = express()
 const db = new sqlite3.Database("./database.sqlite")
+
 /*
     User {
         email
