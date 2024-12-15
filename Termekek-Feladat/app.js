e4eb97018907dcad1561e7b8504446555349be73
b0totmat <t0thm4t3@gmail.com>
2024-12-15 09:33:46 +0100

    Adatbázis

--- Termekek-Feladat/app.js
+++ Termekek-Feladat/app.js
@@ -1,7 +1,25 @@
 import express from 'express'
 import sqlite3 from 'sqlite3'
 
-const app = express()
+const app = express(),
+      db  = sqlite3.Database('./database.sqlite')
+
+/*
+    Product {
+        id
+        name
+        description
+        picture
+        price
+    }
+*/
+
+db.serialize(() => {
+    db.run('DROP TABLE IF EXISTS products')
+    db.run('CREATE TABLE products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, description TEXT, picture TEXT, price TEXT)')
+})
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
